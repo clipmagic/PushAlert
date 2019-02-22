323d0fd428f923e102147290e8d0e24dfbc862c9
clipmagic <admin@clipmagic.com.au>
2019-02-22 17:20:41 +1100

    performance tweak

--- templates/pushalert_endpoint.php
+++ templates/pushalert_endpoint.php
@@ -7,28 +7,29 @@
  * Make a new template file and use it as the "Alternate template file" instead
  */
 
-# Get JSON as a string
-$json_str = @file_get_contents('php://input');
-if (empty($json_str)) return;
-
-
-# Get as an object
-$json_obj = json_decode($json_str);
-
-$paMod = $modules->get("PushAlert");
-
-$subscribers = $page->children("title=$json_obj->subscriber_id");
-if ($subscribers->count > 0)
-    return;
-
 // only save subscription ids for logged in users
 if ($user->isLoggedin()) {
-    $p = new Page();
-    $p->of(false);
-    $p->template = 'pushalert_subscription';
-    $p->parent   = $page->id;
-    $p->title    = $json_obj->subscriber_id;
-    $p->pushalert_user_id = $user->id;
-    $p->save();
+
+    # Get JSON as a string
+    $json_str = @file_get_contents('php://input');
+    if (empty($json_str)) return;
+
+    # Get as an object
+    $json_obj = json_decode($json_str);
+
+    $paMod = $modules->get("PushAlert");
+
+    $subscribers = $page->children("title=$json_obj->subscriber_id");
+    if ($subscribers->count > 0)
+        return;
+
+        $p = new Page();
+        $p->of(false);
+        $p->template = 'pushalert_subscription';
+        $p->parent   = $page->id;
+        $p->title    = $json_obj->subscriber_id;
+        $p->pushalert_user_id = $user->id;
+        $p->save();
+
+    http_response_code(200); // PHP 5.4 or greater
 }
-http_response_code(200); // PHP 5.4 or greater
